dbf299365c46482a20849944b0dfb4567b24a389
Victor Wang <wjp2013@gmail.com>
2015-07-23 15:16:22 +0800

    添加用户登录表单模型

---
+++ app/models/user/login_form.rb
@@ -0,0 +1,28 @@
+class User::LoginForm < ActiveType::Object
+  attribute :username, :string
+  attribute :password, :string
+  attribute :remember_me, :boolean
+
+  validates :username, presence: true
+  validates :password, presence: true
+
+  validate :validate_user_exists
+  validate :validate_password_correct
+
+  def user
+    User.ransack(email_or_mobile_eq: username).result.first
+  end
+
+  private
+
+  def validate_user_exists
+    errors.add(:base, :user_not_found) unless user
+  end
+
+  def validate_password_correct
+    if user && !user.authenticate(password)
+      errors.add(:password, :incorrect)
+    end
+  end
+
+end
