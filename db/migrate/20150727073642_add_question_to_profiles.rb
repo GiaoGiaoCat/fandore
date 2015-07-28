9eff617bb03639bcbb8288a98f59412a6d530935
Victor Wang <wjp2013@gmail.com>
2015-07-28 21:33:22 +0800

    合并 profile 的迁移文件

--- db/migrate/20150727073642_add_question_to_profiles.rb
+++
@@ -1,5 +0,0 @@
-class AddQuestionToProfiles < ActiveRecord::Migration
-  def change
-    add_column :profiles, :question, :string
-  end
-end
--- db/migrate/20150727073803_add_answer_to_profiles.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswerToProfiles < ActiveRecord::Migration
-  def change
-    add_column :profiles, :answer, :string
-  end
-end
