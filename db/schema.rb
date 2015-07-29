74d3bb390b8ac16f3a2169a2c6a2f499fc25d881
Victor Wang <wjp2013@gmail.com>
2015-07-29 11:09:41 +0800

    添加 模型 和 表

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728142514) do
+ActiveRecord::Schema.define(version: 20150729030025) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name",             limit: 255,   default: "",   null: false
@@ -55,6 +55,10 @@ ActiveRecord::Schema.define(version: 20150728142514) do
   end
 
   add_index "properties", ["category"], name: "index_properties_on_category", using: :btree
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string "name", limit: 255
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",              limit: 100
