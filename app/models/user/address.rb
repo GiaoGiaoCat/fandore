133700c128d87326381a1788ca1f6747dc0e8076
Victor Wang <wjp2013@gmail.com>
2015-07-30 01:43:40 +0800

    refactor set default method.

--- app/models/user/address.rb
+++ app/models/user/address.rb
@@ -12,35 +12,34 @@ class User::Address < ActiveRecord::Base
             presence: true,
             format: { with: /\A(13[0-9]|15[0-9]|18[7-8])[0-9]{8}\z/ }
   # callbacks .................................................................
-  after_create :set_default_address
+  after_save :set_default_address
   # scopes ....................................................................
   # other macros (like devise's) ..............................................
   # class methods .............................................................
   # public instance methods ...................................................
   def province_name
-    ChinaCity.get(province)
+    ChinaCity.get(province) rescue ''
   end
 
   def city_name
-    ChinaCity.get(city)
+    ChinaCity.get(city) rescue ''
   end
 
   def district_name
-    ChinaCity.get(district)
+    ChinaCity.get(district) rescue ''
   end
 
-  def default_address
-    self.user.addresses.each do |address|
-      address.update(is_default: false)
-    end
-    self.update(is_default: true)
+  def cancel_default!
+    update_column(:is_default, false)
   end
   # protected instance methods ................................................
   # private instance methods ..................................................
   private
 
   def set_default_address
-    self.update(is_default: true) if self.user.addresses.count == 1
-    default_address if self.is_default
+    if is_default || user.addresses.count == 1
+      user.addresses.each(&:cancel_default!)
+      update_column(:is_default, true)
+    end
   end
 end
