3b42a6361fa2eb963224761143dd0a625cc17e41
Victor Wang <wjp2013@gmail.com>
2015-07-30 00:51:35 +0800

    整理迁移文件

--- db/migrate/20150728063929_create_addresses.rb
+++ db/migrate/20150728063929_create_addresses.rb
@@ -1,15 +1,19 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
-      t.integer :post_code
-      t.string :full_name
+      t.references :user, index: true, foreign_key: true
+
+      t.string :name
+      t.string :zipcode
       t.string :mobile
-      t.string :phone_section
-      t.integer :phone_code
-      t.string :phone_ext
-      t.text :address_detail
       t.string :province
       t.string :district
+      t.string :city
+      t.string :address
+      t.string :phone_area_code
+      t.string :phone
+      t.string :phone_ext_code
+      t.boolean :is_default, default: false
 
       t.timestamps null: false
     end
--- db/migrate/20150728161245_add_is_default_to_address.rb
+++
@@ -1,5 +0,0 @@
-class AddIsDefaultToAddress < ActiveRecord::Migration
-  def change
-    add_column :addresses, :is_default, :boolean, default: false
-  end
-end
