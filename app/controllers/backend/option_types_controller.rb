07b17dab3ed20c4ebf84044b5bb7c709db273749
Victor Wang <wjp2013@gmail.com>
2015-07-30 18:32:48 +0800

    CRUD option types.

---
+++ app/controllers/backend/option_types_controller.rb
@@ -0,0 +1,69 @@
+class Backend::OptionTypesController < Backend::ApplicationController
+
+  skip_before_action :authenticate_user!
+  before_action :load_option_type, only: [:show, :edit, :update, :destroy]
+
+  def index
+    load_option_types
+  end
+
+  def show
+    load_option_type
+  end
+
+  def new
+    build_option_type
+  end
+
+  def create
+    build_option_type
+    save_option_type or render 'new'
+  end
+
+  def edit
+    load_option_type
+    build_option_type
+  end
+
+  def update
+    load_option_type
+    build_option_type
+    save_option_type or render 'edit'
+  end
+
+  def destroy
+    load_option_type
+    @option_type.destroy
+    redirect_to admin_option_types_path
+  end
+
+  private
+
+  def load_option_types
+    @option_types ||= option_type_scope.all
+  end
+
+  def load_option_type
+    @option_type ||= option_type_scope.find(params[:id])
+  end
+
+  def build_option_type
+    @option_type ||= option_type_scope.new
+    @option_type.attributes = option_type_params
+  end
+
+  def save_option_type
+    if @option_type.save
+      redirect_to admin_option_types_path
+    end
+  end
+
+  def option_type_params
+    option_type_params = params[:product_option_type]
+    option_type_params ? option_type_params.permit(:name, :presentation, :position) : {}
+  end
+
+  def option_type_scope
+    Product::OptionType
+  end
+end
--- db/migrate/20150730100644_create_option_types.rb
+++ db/migrate/20150730100644_create_option_types.rb
@@ -1,8 +1,8 @@
 class CreateOptionTypes < ActiveRecord::Migration
   def change
     create_table :option_types do |t|
-      t.string  :name, limit: 100
-      t.string  :presentation, limit: 100
+      t.string  :name
+      t.string  :presentation
       t.integer :position, default: 0, null: false, index: true
 
       t.timestamps null: false
