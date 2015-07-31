32e1ee89f8b6d2928168e052675e49ad32cc36f7
Victor Wang <wjp2013@gmail.com>
2015-08-01 00:30:51 +0800

    修改迁移文件，去掉 id

--- db/migrate/20150729060436_create_properties_prototypes.rb
+++ db/migrate/20150729060436_create_properties_prototypes.rb
@@ -1,6 +1,6 @@
 class CreatePropertiesPrototypes < ActiveRecord::Migration
   def change
-    create_table :properties_prototypes do |t|
+    create_table :properties_prototypes, id: false do |t|
       t.references :property, index: true, foreign_key: true
       t.references :prototype, index: true, foreign_key: true
     end
