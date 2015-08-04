abdf50b5d44dcbd4c29743d8fc100adb2bfc3d15
wildimagine <13932011427@sina.cn>
2015-08-04 22:59:03 +0800

    fix: 添加登录身份验证， 2：将admin登录页面layout改为empty， 3：user迁移文件添加role字段信息，default为1， admin为0

--- app/controllers/frontend/users/sessions_controller.rb
+++ app/controllers/frontend/users/sessions_controller.rb
@@ -1,9 +1,14 @@
 class Frontend::Users::SessionsController < Frontend::ApplicationController
   include Authenticatable
 
+
   private
 
   def redirect_to_url
     redirect_to root_path
+  end
+
+  def authority_verify(user)
+    !user.admin?
   end
 end
