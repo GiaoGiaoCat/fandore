dd0ce4d15f0c6793df9c6ecee9b51b0d924d7955
Ruochen <ruochen@mycolorway.com>
2015-08-09 15:50:39 +0800

    fix bug

--- app/assets/javascripts/backend.js
+++ app/assets/javascripts/backend.js
@@ -1,10 +1,9 @@
-//  require jquery
-//  require jquery_ujs
 //  require turbolinks
 //
 // 不使用 Rails 提供的 jQuery 而使用后端模板提供的
 //= require jquery/jquery-2.1.1.js
-//= require jquery_ujs
+//  require jquery_ujs
+//
 //= require cocoon
 //= require bootstrap-sprockets
 //= require metisMenu/jquery.metisMenu.js
