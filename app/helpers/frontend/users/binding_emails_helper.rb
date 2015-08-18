224e88ae9d71b0d28ac590148439b0847bee9984
geniousli <13932011427@sina.cn>
2015-08-18 22:49:35 +0800

    modify gmail helper mehtod

--- app/helpers/frontend/users/binding_emails_helper.rb
+++ app/helpers/frontend/users/binding_emails_helper.rb
@@ -1,8 +1,6 @@
 module Frontend::Users::BindingEmailsHelper
-  def email_path
-    if current_user && current_user.email
-      current_user.email.to_email_url
-    end
+  def email_path(user)
+    user.email_url
   end
 end
 
--- lib/core_ext/string_ex.rb
+++
@@ -1,9 +0,0 @@
-class String
-  def to_email_url
-    if self.include?("gmail")
-      "http://gmail.google.com/"
-    else
-      "http://mail.#{self.split('@')[1]}"
-    end
-  end
-end
