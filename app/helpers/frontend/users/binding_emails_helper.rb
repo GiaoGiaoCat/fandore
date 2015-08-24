320e8f602b2ca8109b5016bde76ca751fed97da2
wildimagine <13932011427@sina.cn>
2015-08-24 22:52:31 +0800

    fix: 跳转gmail的bug修改

--- app/helpers/frontend/users/binding_emails_helper.rb
+++ app/helpers/frontend/users/binding_emails_helper.rb
@@ -1,6 +1,11 @@
 module Frontend::Users::BindingEmailsHelper
   def email_path(user)
-    user.email_url
+    email = user.email
+    if email.present?
+      email.include?("gmail") ? "http://gmail.google.com/" :  "http://mail.#{email.split('@')[1]}"
+    else
+      users_profile_edit_path
+    end
   end
 end
 
--- config/initializers/require_core_ext.rb
+++
@@ -1,3 +0,0 @@
-Dir[Rails.root + 'lib/core_ext/*.rb'].each do |file|
-  require file
-end
