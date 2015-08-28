d849d35b92e1df8b42920ac8790e3db8282d5f32
Victor Wang <wjp2013@gmail.com>
2015-08-28 23:31:32 +0800

    创建订单可以保存地址

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ActiveRecord::Base
   include OrderStates
   # constants .................................................................
   # related macros ............................................................
-  attr_accessor :use_billing
+  attr_accessor :use_shipping
   # relationships .............................................................
   # belongs_to :store
   belongs_to :user
@@ -33,7 +33,7 @@ class Order < ActiveRecord::Base
 
   # validations ...............................................................
   # callbacks .................................................................
-  before_validation :clone_billing_address, if: :use_billing?
+  before_validation :clone_shipping_address, if: :use_shipping?
   before_create :create_token
   before_create :link_by_email
   # scopes ....................................................................
@@ -45,6 +45,10 @@ class Order < ActiveRecord::Base
   # accepts_nested_attributes_for :shipments
   # class methods .............................................................
   # public instance methods ...................................................
+  def shipping_address
+    super || build_shipping_address(user_id: user_id)
+  end
+
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       #NOTE: 这里应该改成 购物车假删除, 并且购物车删除，不删除关联的 line items
@@ -60,11 +64,11 @@ class Order < ActiveRecord::Base
     self.email = user.email if self.user
   end
 
-  def clone_billing_address
-    if billing_address && shipping_address.nil?
-      self.shipping_address = billing_address.clone
+  def clone_shipping_address
+    if shipping_address && billing_address.nil?
+      self.billing_address = shipping_address.clone
     else
-      self.shipping_address.attributes = billing_address.attributes.except('id', 'updated_at', 'created_at')
+      self.billing_address.attributes = shipping_address.attributes.except('id', 'updated_at', 'created_at')
     end
     true
   end
@@ -76,9 +80,9 @@ class Order < ActiveRecord::Base
     end
   end
 
-  def use_billing?
-    # @use_billing == true || @use_billing == 'true' || @use_billing == '1'
-    @use_billing == true
+  def use_shipping?
+    # @use_shipping == true || @use_shipping == 'true' || @use_shipping == '1'
+    @use_shipping == true
   end
 
 end
