b13f27304607d32c6374a72810f9cc9605a19dc5
Victor Wang <wjp2013@gmail.com>
2015-09-28 13:17:54 +0800

    验证码改为纯数字

--- config/initializers/simple_captcha_reloaded.rb
+++ config/initializers/simple_captcha_reloaded.rb
@@ -1,1 +1,14 @@
 require 'simple_captcha_reloaded/adapters/simple_form'
+
+SimpleCaptchaReloaded::Config.tap do |config|
+  # config.captcha_path = '/simple_captcha'
+  config.image = SimpleCaptchaReloaded::Image.new(
+      # implode: :medium,
+      # distortion: :medium,
+      # image_styles: {custom: ['-fill darkblue', '-solarize 50', '-background black']},
+      noise: 3,
+      size: '100x30',
+  )
+  config.characters = %w[0 1 2 3 4 5 6 7 8 9]
+  config.length = 6
+end
