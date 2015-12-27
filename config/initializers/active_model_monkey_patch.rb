8ef71c4357a4e25f267d4fa4f9e7db0d243ca025
Victor Wang <wjp2013@gmail.com>
2015-12-27 14:39:30 +0800

    错误信息处理

--- config/initializers/active_model_monkey_patch.rb
+++ config/initializers/active_model_monkey_patch.rb
@@ -1,10 +1,14 @@
 # generate error json for front-end api
 module ActiveModel
   class Errors
-    def to_formatted_json(as: nil)
+    def to_formatted_json(as: nil, accepts_nested_attributes: false)
       hash = self.messages.each_with_object({}) do |(attribute, array), messages|
         # key = "field-#{as.to_s.presence || @base.model_name.singular}-#{attribute.to_s.gsub(/\./, '-')}"
-        key = "#{as.to_s.presence || @base.model_name.singular}[#{attribute.to_s.gsub(/\./, '-')}]"
+        if accepts_nested_attributes
+          key = "#{as.to_s.presence || @base.model_name.singular}[#{attribute.to_s.gsub(/\./, '_attributes][')}]"
+        else
+          key = "#{as.to_s.presence || @base.model_name.singular}[#{attribute.to_s.gsub(/\./, '-')}]"
+        end
         messages[key] = array
       end
       hash.to_json.html_safe
