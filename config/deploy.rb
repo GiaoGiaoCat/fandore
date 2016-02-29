e8cf650185b318333a38fc294fc58ac0098bb7fb
Victor Wang <wjp2013@gmail.com>
2016-02-29 23:11:08 +0800

    部署在 Ruby 2.3.0 版本下

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[ruby-2.2.2@rails4]'
+  invoke :'rvm:use[ruby-2.3.0@rails4]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
